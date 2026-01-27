c9218c71a4f186552108bfc936e19ab6edac35c4
DonDev-v <dennis2327@naver.com>
2026-01-27 12:59:35 +0900

    feat: 뷰 초안 작성

---
+++ Dorgu/Dorgu/CameraView.swift
@@ -0,0 +1,14 @@
+//
+//  CameraView.swift
+//  Dorgu
+//
+//  Created by 이돈혁 on 1/27/26.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
---
+++ Dorgu/Dorgu/MessageView.swift
@@ -0,0 +1,14 @@
+//
+//  MessageView.swift
+//  Dorgu
+//
+//  Created by 이돈혁 on 1/27/26.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
